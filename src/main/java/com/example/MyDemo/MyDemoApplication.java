ff62ad44ff626623d56aad5d9a3493c27fe40bfa
lovesinghonline <lovesinghonline@gmail.com>
2023-04-24 18:27:42 -0400

    swagger commit

--- src/main/java/com/example/MyDemo/MyDemoApplication.java
+++ src/main/java/com/example/MyDemo/MyDemoApplication.java
@@ -2,10 +2,11 @@ package com.example.MyDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 
 public class MyDemoApplication {
 
--- src/main/java/com/example/MyDemo/repository/UserRepository.java
+++ src/main/java/com/example/MyDemo/repository/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public class UserRepository {
-    public List<User> userList= new ArrayList();
+    public List<User> userList= new ArrayList<>();
     public User getUser(){
        User user =new User("love","canada",28);
         return user;
